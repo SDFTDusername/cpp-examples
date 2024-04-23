3cf7a3b2423a89fa54a2dd4deda41de64d2a25dd
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2024-04-23 20:07:06 +0300

    Create references.cpp

---
+++ examples/references.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // & are references.
+    string original = "pizza";
+    string &refr = original;
+    cout << original << endl; // pizza
+    refr = "banana"; // Since refr is a reference to original, it directly edits original
+    cout << original << endl; // banana
+
+    // & can also get the memory address.
+    cout << &original << endl; // 0x61fde0
+
+    // Read more at https://www.w3schools.com/cpp/cpp_references.asp
+
+    return 0;
+}
